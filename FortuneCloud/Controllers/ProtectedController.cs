533833f2d016f4fc480feab4e63a15404affda62
DwipayanT <daipayan.tripathy@idsnext.com>
2017-04-06 18:02:31 +0530

    SSOJWT Initial Commit

---
+++ FortuneCloud/Controllers/ProtectedController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace FortuneCloud.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/protected")]
+    public class ProtectedController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<object> Get()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            return identity.Claims.Select(c => new
+            {
+                Type = c.Type,
+                Value = c.Value
+            });
+        }
+        public string Get(int id)
+        {
+            return "Working";
+        }
+    }
+   
+}
---
+++ FortuneCloud/Startup.cs
@@ -0,0 +1,63 @@
+﻿using IdentityProvider;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace FortuneCloud
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+           
+            app.UseWebApi(config);
+
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var issuer = "http://jwtauthzsrv.azurewebsites.net";
+            var audience = "099153c2625149bc8ecb3e85e03f0022";
+            var Base64Secret = "IxrAjDoa2FqElO7IhrSrUJELhUckePEPVpaePlS_Xaw";
+            var secret = TextEncodings.Base64Url.Decode(Base64Secret);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    },
+                    Provider = new OAuthBearerAuthenticationProvider
+                    {
+                        OnValidateIdentity = context =>
+                        {
+                            context.Ticket.Identity.AddClaim(new System.Security.Claims.Claim("newCustomClaim", "newValue"));
+                            return Task.FromResult<object>(null);
+                        }
+                    }
+                });
+
+        }
+    }
+}
